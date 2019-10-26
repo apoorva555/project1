d05010791167bb591b00c2331398ce7af85da924
apoorva555 <apoorvagowdamr@gmail.com>
2019-10-26 01:37:29 -0700

    day3startup

---
+++ day3.js
@@ -0,0 +1,30 @@
+function myFunction(elem) {
+
+    console.log("alert")
+     alert("this is alert");
+}
+function myFunction1(eles) {
+
+    console.log("alert1")
+    alert("yhis is double click");
+}
+function myFunction2(elem) {
+
+    alert("thos is mouse over");
+}
+function myFunction3(elem,eles) {
+ console.log("alerting")
+ console.log(eles.clientX+ ': '+eles.clientY);
+    debugger
+}
+function myfunction(elem) {
+      alert("This is 2 alert");
+ }
+ function sayHi(elem) {
+     
+    //  alert ( " this is child 2")
+     console.log(event.target.value)
+     document.getElementById('changed').innerHTML=event.target.value;
+    
+ }
+ 
